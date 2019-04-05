219edc0585534a237b47d8beeacec9fdc841d9fe
RaymondDashWu <raymond31670@hotmail.com>
2019-04-04 21:19:30 -0700

    [ADD] Factorial iterative finished

--- recursion.py
+++ recursion.py
@@ -9,20 +9,29 @@ def factorial(n):
         raise ValueError('factorial is undefined for n = {}'.format(n))
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
+    # return factorial_recursive(n)
 
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
     # TODO: Annotate functions with complexity analysis of running time and space (memory)
-    pass
+    total = 1
+    for num in range(1, n):
+        if n > 1:
+            print('HERE DUM DUM', num)
+            # num + 1 is used to account for range going up to but not hitting n
+            total *= (num + 1)
+            print('LOOK', total)
+    return total
+
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
 
 
 def factorial_recursive(n):
     # TODO: Annotate functions with complexity analysis of running time and space (memory)
+    # Time - O(1) Space - O(n)
     # check if n is one of the base cases
     if n == 0 or n == 1:
         return 1
