697cdc36224b96e7603623b2871c87ed5e142be5
RaymondDashWu <raymond31670@hotmail.com>
2019-04-05 00:18:04 -0700

    [ADD] linear search recursive function

--- search.py
+++ search.py
@@ -2,7 +2,6 @@
 
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
-    # TODO: Annotate functions with complexity analysis of running time and space (memory)
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
     return linear_search_iterative(array, item)
@@ -10,7 +9,6 @@ def linear_search(array, item):
 
 
 def linear_search_iterative(array, item):
-    # TODO: Annotate functions with complexity analysis of running time and space (memory)
     # loop over all array values until item is found
     for index, value in enumerate(array):
         if item == value:
@@ -20,15 +18,18 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: Annotate functions with complexity analysis of running time and space (memory)
+    # Time - O(1) Space - O(n)
     # TODO: implement linear search recursively here
-    pass
+    if array[index] == item:
+        return index
+    else:
+        return linear_search_recursive(array, item, index + 1)
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
 
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
-    # TODO: Annotate functions with complexity analysis of running time and space (memory)
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
     return binary_search_iterative(array, item)
@@ -38,7 +39,8 @@ def binary_search(array, item):
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
     # TODO: Annotate functions with complexity analysis of running time and space (memory)
-    pass
+    for index, value in enumerate:
+        if 
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
