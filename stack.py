1b3efadfb98801d363a24db39ec4f2be5bdf1821
RaymondDashWu <raymond31670@hotmail.com>
2019-04-16 23:20:18 -0700

    [ADD] is_empty & length function

--- stack.py
+++ stack.py
@@ -21,11 +21,11 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list is not None
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
